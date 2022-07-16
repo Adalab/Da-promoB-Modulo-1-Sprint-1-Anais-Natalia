68aa9b1a85b0b59bdb8dc49a8b4d14328d92f434
anaisbonamy <anaisbonamy@hotmail.com>
2022-07-17 00:52:18 +0200

    python: diccionarios, sql: consultas basicas 1

--- PP_SQL_Consultas_Basicas1_Anais_Natalia.sql
+++ PP_SQL_Consultas_Basicas1_Anais_Natalia.sql
@@ -1,16 +1,44 @@
 USE northwind;
 
+|#1
 SELECT employee_id, last_name, first_name 
 FROM employees;
 
+#2
 SELECT unit_price
 FROM products
 WHERE unit_price= 0<5;
 
- 18, 19 o 20 dólares (un rango muy concreto de precios 
-del que la empresa quiere maximizar sus ventas en un futuro).
-
+#3
 SELECT unit_price
 FROM products
-WHERE unit_price= <18,19, 20>;
+WHERE unit_price= 18
+OR unit_price= 19
+OR unit_price= 20;
 
+#4
+select unit_price
+from products
+where unit_price >= 15
+and unit_price <= 50;
+
+#5
+select unit_price
+from products 
+where unit_price is null;
+
+#6
+SELECT unit_price
+FROM products
+WHERE unit_price <15
+and product_id <10;
+
+#7
+SELECT unit_price
+FROM products
+WHERE NOT unit_price <15
+and not product_id <10;
+
+#8
+select ship_country
+from orders;
